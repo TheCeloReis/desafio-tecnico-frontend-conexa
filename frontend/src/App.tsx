f1af8efd0576248bd8de357010010da095f81c80
Celo Reis <m.reis@portaltelemedicina.com.br>
2022-08-27 14:21:33 -0300

    chore: basic routes setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,11 @@
-import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-red-500">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from "../components/Button";
+
+function Login(): JSX.Element {
+  return <Button fullWidth>Test</Button>;
+}
+
+export default Login;
