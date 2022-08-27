b22929d0f094725b75754e889bce0e079242dbc8
Celo Reis <m.reis@portaltelemedicina.com.br>
2022-08-27 14:21:05 -0300

    chore: tailwind theme config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Nunito", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          dark: "#1C307F",
+          medium: "#2E50D4",
+          light: "#4F81FF",
+        },
+        gray: {
+          dark: "#575453",
+          medium: "#999392",
+          light: "#dad2d0",
+        },
+      },
+    },
   },
   plugins: [],
 };
